4fe7dd0428f5114706d2f4fdbc87fe0850cc0c2e
Rafael Corrêa Vieira <rafacorreavieira2020@gmail.com>
2021-12-28 23:44:37 -0300

    new att

---
+++ adicionarDepois.kts
@@ -0,0 +1,12 @@
+/*
+qualquer tipo pode ser nulo porem isso deve ser explicitado na declaração da variavel atraves do (?)
+var month:Int? = null
+ */
+
+fun main() {
+    var a:Int = 1
+    var b:Int = 2
+
+    var c:Int = a.plus(b)
+    println(c)
+}
